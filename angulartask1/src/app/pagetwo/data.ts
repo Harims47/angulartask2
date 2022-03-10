1084d843c5985a3e24045a4dc3d994f31e8e1b23
Harinarayanan.A <8810060@INDUS.LOCAL>
2022-03-10 14:22:10 +0530

    changes 8 completed

--- angulartask1/src/app/pagetwo/data.ts
+++ angulartask1/src/app/pagetwo/data.ts
@@ -1,49 +1,9 @@
 
   export let cardData: Object[] = [
-    {
-      'Id': 'Abirami',
-      'Title': 'Abirami',
-      'Status': 'TO DO',
-      'qualification':'Bsc.IT',
-      'Role': 'Angular Developer',
-      'Priority': 'High',
-      'experience': '1',
-      'RankId': 4,
-      'Assignee': 'HR Recuirter'
-    },
-    {
-      'Id': 'Hari',
-      'Title': 'Hari',
-      'Status': 'Face to Face',
-      'qualification':'Bsc.IT',
-      'Role': 'React Developer',
-      'Priority': 'High',
-      'experience': 'Fresher',
-      'RankId': 3,
-      'Assignee': 'HR Recuirter'
-    },
-    {
-      'Id': 'Karthik',
-      'Title': 'Karthik',
-      'Status': 'Technical',
-      'qualification':'Bsc.IT',
-      'Role': 'UI/UX Designer',
-      'Priority': 'High',
-      'experience': '3',
-      'RankId': 2,
-      'Assignee': 'HR Recuirter'
-    },
-    {
-      'Id': 'Santhosh',
-      'Title': 'Santhosh',
-      'Status': 'Offer Accepted',
-      'qualification':'Bsc.IT',
-      'Role': 'Front end developer',
-      'Priority': 'High',
-      'experience': '8',
-      'RankId': 8,
-      'Assignee': 'HR Recuirter'
-    },
+    { 'Id': 'Abirami', 'Title': 'Abirami', 'Status': 'TO DO', 'qualification':'Bsc.IT', 'Role': 'Angular Developer', 'Priority': 'High', 'experience': '1', 'RankId': 4, 'Assignee': 'HR Recuirter' },
+    { 'Id': 'Hari', 'Title': 'Hari', 'Status': 'Face to Face', 'qualification':'Bsc.IT', 'Role': 'React Developer', 'Priority': 'High', 'experience': 'Fresher', 'RankId': 3, 'Assignee': 'HR Recuirter' },
+    { 'Id': 'Karthik', 'Title': 'Karthik', 'Status': 'Technical', 'qualification':'Bsc.IT', 'Role': 'UI/UX Designer', 'Priority': 'High', 'experience': '3', 'RankId': 2, 'Assignee': 'HR Recuirter' },
+    { 'Id': 'Santhosh', 'Title': 'Santhosh', 'Status': 'Offer Accepted', 'qualification':'Bsc.IT', 'Role': 'Front end developer', 'Priority': 'High', 'experience': '8', 'RankId': 8, 'Assignee': 'HR Recuirter' },
   ];
   
  
